a9d067fbd35c4c91e53b68b07273635762805a59
AYUSH0613 <ayushparoliya2004@gmail.com>
2025-07-08 23:53:48 +0530

    Add getWatchedHistory endoint

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,6 +1,6 @@
 import mongoose, { Schema } from "mongoose";
 
-const subcriptionSchema = new Schema({
+const subscriptionSchema = new Schema({
     subscriber: {
         type: Schema.Types.ObjectId,
         ref: "User"
@@ -12,4 +12,4 @@ const subcriptionSchema = new Schema({
 }, {timestamps: true})
 
 
-export const Subscription = mongoose.model("Subscription", subcriptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema)
